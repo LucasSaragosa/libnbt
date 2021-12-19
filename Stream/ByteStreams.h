edaa33e33a85fddc503391c70ccc8d43bb1faf14
Lucas Saragosa <62749084+LucasSaragosa@users.noreply.github.com>
2021-12-19 17:35:58 +0000

    Create ByteStreams.h

---
+++ Stream/ByteStreams.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+
+#define LITTLE_ENDIAN 1
+#define BIG_ENDIAN 2
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8 endian;
